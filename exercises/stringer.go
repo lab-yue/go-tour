326abda41e1cccc32ec8407cf4a9c6db529f6954
yue.lab <gitlab@rainy.me>
2019-02-10 19:48:45 +0900

    fix stringer.go

--- exercises/stringer.go
+++ exercises/stringer.go
@@ -7,7 +7,7 @@ import (
 type IPAddr [4]byte
 
 func (ip IPAddr) String() string {
-	return fmt.Sprintf("%d.%d.%d.%d", ip[0], ip[1], ip[3], ip[3])
+	return fmt.Sprintf("%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3])
 }
 
 func main() {
